b272295e8f7af73e517aea8215c8beccedda6ff4
csvidit <63170797+csvidit@users.noreply.github.com>
2025-03-22 16:37:35 -0400

    refactor: Remove NeonAdapter and Pool instantiation from auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,11 @@
-import NextAuth from "next-auth"
-import NeonAdapter from "@auth/neon-adapter"
-import { Pool } from "@neondatabase/serverless"
-import Google from "next-auth/providers/google"
- 
-// *DO NOT* create a `Pool` here, outside the request handler.
- 
-export const { handlers, auth, signIn, signOut } = NextAuth((
-) => {
-  // Create a `Pool` inside the request handler.
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+import NextAuth from "next-auth";
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
+import Google from "next-auth/providers/google";
+
+
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
   return {
-    adapter: NeonAdapter(pool),
     providers: [Google],
-  }
-})
+  };
+});
